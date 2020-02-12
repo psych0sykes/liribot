df9a9adb61af1218ece5591d915d09862da3a9dc
psych0sykes <blakesykes13@gmail.com>
2020-02-11 23:45:29 -0700

    Cleaned up some bugs and tested app.

--- liri.js
+++ liri.js
@@ -5,7 +5,11 @@ var Spotify = require("node-spotify-api");
 var command = process.argv[2];
 var apiParam = process.argv[3];
 var spotify = new Spotify(keys.spotify);
+var queryUrlOMDB = "http://www.omdbapi.com/?t=" + apiParam + "&y=&plot=short&apikey=trilogy";
+var axios = require("axios");
 var fs = require("fs");
+var queryUrlBIT = "https://rest.bandsintown.com/artists/" + apiParam + "/events?app_id=codingbootcamp";
+var moment = require("moment");
 
 function app()
 {
@@ -26,7 +30,7 @@ function app()
             spotifyThis();
             break;
 
-        case "movie-this":node
+        case "movie-this":
             omdb();
             break;
 
@@ -57,16 +61,16 @@ function spotifyThis()
             }
             for (var i = 0; i < data.tracks.items.length; i++)
             {
-                var artist1 = "Artist: " + data.tracks.items[i].artists[0].name;
+                var artist1 = "artist: " + data.tracks.items[i].artists[0].name;
                 console.log(artist1);
 
-                var song = "Song: " + data.tracks.items[i].name;
+                var song = "song: " + data.tracks.items[i].name;
                 console.log(song);
 
-                var album = "Album: " + data.tracks.items[i].album.name;
+                var album = "album: " + data.tracks.items[i].album.name;
                 console.log(album);
 
-                var preview = "Preview Link: " + data.tracks.items[i].preview_url;
+                var preview = "preview link: " + data.tracks.items[i].preview_url;
                 console.log(preview);
 
                 var lineBreak1 = "\n------------------------------\n"
@@ -95,7 +99,7 @@ function whatItSays()
      command = data1[0];
      apiParam = data1[1];
  
-     spotify();
+     spotifyThis();
  });
 }
 
@@ -161,6 +165,58 @@ function bands()
 function omdb()
 {
     console.log("omdb yo")
+    testAPI();
+
+    axios.get(queryUrlOMDB).then(
+        function(response)
+        {
+            var movieTitle = "title: " + response.data.Title;
+            console.log(movieTitle);
+
+            var releaseYear = "release: " + response.data.Year;
+            console.log(releaseYear);
+
+            var ratingIMDB = "IMDB rating: " + response.data.imdbRating;
+            console.log(ratingIMDB);
+
+            var ratingRT = "rotten tomatoes rating: " + response.data.Ratings[1].Value;
+            console.log(ratingRT);
+
+            var produced = "country produced: " + response.data.Country;
+            console.log(produced);
+
+            var plot = "plot: " + response.data.Plot;
+            console.log(plot);
+
+            var starring = "starring: " + response.data.Actors;
+            console.log(starring);
+
+            dataOMDB = "OMDB DATA:" + "\n" + movieTitle + "\n" + releaseYear + "\n" + ratingIMDB + "\n" + ratingRT + "\n" + produced + "\n" + plot + "\n" + starring;
+
+            logit(dataOMDB);
+        })
+        .catch(function(error)
+        {
+            if (error.response) 
+            {
+                console.log("error error error");
+                console.log(error.response.status);
+
+            } 
+        
+            else if (error.request)
+            {
+            console.log(error.request);
+            } 
+        
+            else
+            {
+            console.log("error", error.message);
+            }
+    
+            console.log(error.config);
+        }
+    );
 }
 
 function logIt(data)
@@ -179,8 +235,18 @@ function testAPI()
     {
         switch(command)
         {
+            case "concert-this":
+                apiParam = "Porches";
+                queryUrlBIT = "https://rest.bandsintown.com/artists/" + apiParam + "/events?app_id=codingbootcamp";
+                break;
+
             case "spotify-this-song":
                 apiParam = "NEW MAGIC WAND";
+                break;
+
+            case "movie-this":
+                apiParam = "Star Wars";
+                queryUrlOMDB = "http://www.omdbapi.com/?t=" + apiParam + "&y=&plot=short&apikey=trilogy";
                 break;
         }
     }
